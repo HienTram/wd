a3cadd460fd613713d3c5666116fb5ca1044b6a7
HienTram <141378071+HienTram@users.noreply.github.com>
2023-08-04 09:00:51 +0700

    Add files via upload

---
+++ js/control.js
@@ -0,0 +1,9 @@
+// JavaScript Document 
+var typed = new Typed('.element', {
+    strings: ["Cartoon Network"],
+    typeSpeed:100,
+    backSpeed:100,
+  loop:true,
+});
+
+$('.counter').countUp();
